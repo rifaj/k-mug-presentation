80b9f180d3fdc3f22d5479ce81009ff3bc55a05b
Rifaj Aboobacker <rifaj.aboobacker@redblacksoftware.com>
2017-05-02 14:53:10 +0530

    Added first name

--- K-Mug/Models/UserModel.cs
+++ K-Mug/Models/UserModel.cs
@@ -18,5 +18,7 @@ namespace K_Mug.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+		
+		 public string FirstName { get; set; }
     }
 }
